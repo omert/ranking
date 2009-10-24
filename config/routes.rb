1bbae41bfa83b832581fc382402e8a2b10061650
Omer Tamuz <tamuz@sibyllic2.(none)>
2009-10-24 14:20:51 +0200

    seeding database with movies

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :users
+
+  map.resource :session
+
   map.resources :rankings
 
   map.resources :criterions
@@ -50,4 +54,9 @@ ActionController::Routing::Routes.draw do |map|
   # consider removing or commenting them out if you're using named routes and resources.
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
+
+  map.activate '/activate/:activation_code', :controller => 'users', :action => 'activate', :activation_code => nil
+  map.signup '/signup', :controller => 'users', :action => 'new'
+  map.login '/login', :controller => 'sessions', :action => 'new'
+  map.logout '/logout', :controller => 'sessions', :action => 'destroy'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #   
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Major.create(:name => 'Daley', :city => cities.first)
+
+require 'cgi'
+require 'open-uri'
+require 'nokogiri'
+
+if Movie.count == 0
+  imdb_url = 'http://www.imdb.com/chart/top'
+  imdb_page = open(imdb_url).read 
+  imdb_parsed = Nokogiri::HTML.parse(imdb_page)
+  imdb_parsed.css('#main table a').each do |link|
+    Movie.create :name => link.content, :imdb_link => link.attribute('href').to_s
+  end
+end
